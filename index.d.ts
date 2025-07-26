856c8f6b20e1107caeaf0ebca389c2c521a459b5
Oli <oliverfwales@gmail.com>
2025-07-26 12:56:15 +0100

    Add TS type declarations (#129)

---
+++ index.d.ts
@@ -0,0 +1,33 @@
+declare module "whois" {
+  interface WhoisOptions {
+    server?: string | { host: string; port: number };
+    follow?: number;
+    timeout?: number;
+    verbose?: boolean;
+    bind?: string;
+    proxy?: {
+      ipaddress: string;
+      port: number;
+      type?: number;
+    };
+    punycode?: boolean;
+    encoding?: string;
+  }
+
+  interface WhoisResult {
+    server: string;
+    data: string;
+  }
+
+  type WhoisCallback = (
+    err: Error | null,
+    data: string | WhoisResult[]
+  ) => void;
+
+  function lookup(addr: string, callback: WhoisCallback): void;
+  function lookup(
+    addr: string,
+    options: WhoisOptions,
+    callback: WhoisCallback
+  ): void;
+}
